8898dace85eef3d711b40e6b86b32ee5e1d5a874
Ben <nor15002@byui.edu>
2017-07-08 21:45:23 -0600

    no

--- Overthrow/app/src/main/java/com/example/ben/overthrow/MainMenu.java
+++ Overthrow/app/src/main/java/com/example/ben/overthrow/MainMenu.java
@@ -1,7 +1,10 @@
 package com.example.ben.overthrow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        Button btn = (Button)findViewById(R.id.btnPlay);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenu.this, OptionMenu.class));
+            }
+        });
+    }
+    public void setActivityBackgroundColor(int color) {
+        View view = this.getWindow().getDecorView();
+        view.setBackgroundColor(color);
     }
 }
---
+++ Overthrow/app/src/main/java/com/example/ben/overthrow/OptionMenu.java
@@ -0,0 +1,16 @@
+package com.example.ben.overthrow;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class OptionMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option_menu);
+    }
+}
