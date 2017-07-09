4f6878f836c77a2f737000e89bf38d6e40f98827
Ben <nor15002@byui.edu>
2017-07-08 22:02:41 -0600

    no

--- Overthrow/app/src/main/java/com/example/ben/overthrow/GameBoard.java
+++ Overthrow/app/src/main/java/com/example/ben/overthrow/GameBoard.java
@@ -2,6 +2,8 @@ package com.example.ben.overthrow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class GameBoard extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class GameBoard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_board);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layoutBoard);
+        //layout.setOrientation(LinearLayout.VERTICAL);
+        for (int i = 0; i < 3; i++) {
+            LinearLayout row = new LinearLayout(this);
+            row.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+            for (int j = 0; j < 4; j++) {
+                Button btnTag = new Button(this);
+                btnTag.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                btnTag.setText("Button " + (j + 1 + (i * 4)));
+                btnTag.setId(j + 1 + (i * 4));
+                row.addView(btnTag);
+            }
+            layout.addView(row);
+        }
     }
 }
