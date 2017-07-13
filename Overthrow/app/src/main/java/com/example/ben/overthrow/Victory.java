7ad58863ff73dc707852349678f306ace3408e05
Ben <nor15002@byui.edu>
2017-07-12 22:50:33 -0600

    Graphical changes to UI.

--- Overthrow/app/src/main/java/com/example/ben/overthrow/Victory.java
+++ Overthrow/app/src/main/java/com/example/ben/overthrow/Victory.java
@@ -1,7 +1,10 @@
 package com.example.ben.overthrow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Victory extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class Victory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_victory);
+        Button btnMenu = (Button) findViewById(R.id.btnHome);
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Victory.this, MainMenu.class));
+                finish();
+            }
+        });
     }
 }
